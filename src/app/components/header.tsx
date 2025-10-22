e8fba79bba548f390f9ef901b4910e7edb7037b3
xupie <81492239+Xupie@users.noreply.github.com>
2025-10-22 19:30:28 +0300

    global styles, improve header & themeswitch

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -19,15 +19,15 @@ export default function Header() {
     };
 
     return (
-        <header className='dark:bg-black'>
+        <header>
             <nav className='w-full flex flex-col md:flex-row justify-between items-center'>
 
                 {/* desktop nav */}
                 <div className='hidden md:flex px-20 py-4 w-full items-center justify-between'>
                     <ul className='flex gap-8'>
                         {navLinks.map(({ href, label }) => (
-                            <li key={label} className='hover'>
-                                <Link href={href}>{label}</Link>
+                            <li key={label}>
+                                <Link className='nav-link' href={href}>{label}</Link>
                             </li>
                         ))}
                     </ul>
@@ -40,9 +40,8 @@ export default function Header() {
 
                 {/* Mobile menu */}
                 <div className='relative md:hidden w-full px-4'>
-
                     <button
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        onClick={() => handleClick()}
                         className={`ms-auto mt-2 items-center  ${isMenuOpen ? 'rotate-90' : 'rotate-0'} transition delay-50 duration-300 `}
                         aria-controls='mobile-nav'
                         aria-expanded={isMenuOpen}
@@ -56,8 +55,8 @@ export default function Header() {
                             className={`w-full mb-4 flex flex-col md:hidden overflow-hidden`}
                         >
                             {navLinks.map(({ href, label }) => (
-                                <li key={label} className='hover h-10 flex items-center'>
-                                    <Link className='w-full' onClick={() => setIsMenuOpen(false)} href={href}>{label}</Link>
+                                <li key={label} className='h-10 flex items-center'>
+                                    <Link className='w-full nav-link' onClick={() => handleClick()} href={href}>{label}</Link>
                                 </li>
                             ))}
                             <div className='flex ms-auto items-center gap-4'>

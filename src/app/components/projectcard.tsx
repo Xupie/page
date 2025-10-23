fe55862f4475f4334bb3f96b75ecac50c5c782fc
xupie <81492239+Xupie@users.noreply.github.com>
2025-10-24 01:00:02 +0300

    source optional

--- src/app/components/projectcard.tsx
+++ src/app/components/projectcard.tsx
@@ -1,36 +1,38 @@
-"use client";
-
 import Image from "next/image";
 import type { FC } from "react";
 
 interface ProjectCardProps {
   project: string;
-  img_url: string;
+  img_src: string;
   img_alt: string;
-  source: string;
+  source: string | null;
 }
 
 const ProjectCard: FC<ProjectCardProps> = ({
   project,
-  img_url = "",
-  img_alt = "",
-  source = "",
+  img_src,
+  img_alt,
+  source = null,
 }) => {
   return (
     <div className="px-4 border-2 rounded-md shadow-2xl">
       <h2 className="text-lg font-semibold mb-2">{project}</h2>
       <div className="relative w-full h-32 mb-3">
         <Image
-          src={img_url}
-          alt={img_alt}
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          loading={`lazy`}
+          src={img_src} 
+          alt={img_alt} 
+          fill={true} 
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
+          loading={"lazy"}
         />
       </div>
-      <a href={source} target="_blank" rel="noopener noreferrer">
-        Source
-      </a>
+      {
+        source === null ? (<></>) : (
+          <a href={source} target="_blank" rel="noopener noreferrer">
+            Source
+          </a>
+        )
+      }
     </div>
   );
 };

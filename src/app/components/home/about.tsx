9689e8cf7df4e2ac574e32e731709fa0f84390b3
xupie <81492239+Xupie@users.noreply.github.com>
2025-10-24 01:19:05 +0300

    about me

--- src/app/components/home/about.tsx
+++ src/app/components/home/about.tsx
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <section>
             <h1 className="text-center text-4xl font-sans font-semibold my-4" id="about">About me</h1>
+            <p>Software Developer</p>
         </section>
     );
 }
